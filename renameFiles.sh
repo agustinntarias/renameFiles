1230532cafbea1791e00922d48d3911d84241bca
gustin33 <gustin444@gmail.com>
2020-05-21 23:32:30 -0300

    Added executable command as a comment

--- renameFiles.sh
+++ renameFiles.sh
@@ -1,14 +1,14 @@
-renameFiles () {
-	for file in "$1"/*
-	do 
-		exp1='\(^.*\)\([^0-9]\)\([0-9]\)$'
-		exp2='\(^.*\)\([^0-9]\)\([0-9]\{2\}\)$'
-	
-		new_name="$(echo $file | sed "/$exp1/ s/$exp1/\1\200\3/; /$exp2/ s/$exp2/\1\20\3/")"
-		if [ $new_name != $file ]
-		then
-			echo "Changing $file to  $new_name"
-			mv "$file" "$new_name"
-		fi
-	done
-}
+#!/bin/bash
+# remember to give make file executable with chmod +x ./renameFiles.sh
+for file in "$1"/*
+do 
+	exp1='\(^.*\)\([^0-9]\)\([0-9]\)$'
+	exp2='\(^.*\)\([^0-9]\)\([0-9]\{2\}\)$'
+
+	new_name="$(echo $file | sed "/$exp1/ s/$exp1/\1\200\3/; /$exp2/ s/$exp2/\1\20\3/")"
+	if [ $new_name != $file ]
+	then
+		echo "Changing $file to  $new_name.."
+		mv "$file" "$new_name"
+	fi
+done
